bbe7f8695f2bd7a36e8b6090998151632c75c92e
Carde186 <lorenzocardellini86@gmail.com>
2025-03-11 22:24:22 +0100

    Add files via upload

---
+++ client/src/main/resources/BookTest.java
@@ -0,0 +1,10 @@
+public class BookTest extends TestCase {
+	public void testGet() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:6127/api").path("books").path("1");
+		Response response = target.request().get();
+		System.out.println("Response code: " + response.getStatus());
+		Book book = response.readEntity(Book.class);
+		System.out.println("Title: " + book.getTitle());
+	}
+}
